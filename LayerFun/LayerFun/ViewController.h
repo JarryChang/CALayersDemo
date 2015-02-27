c1f7d77cd6e7028a68930a535f6c37b937e41d06
changyou <changyou0730@gmail.com>
2015-02-27 21:41:21 +0800

    CALayers Tutorial for iOS: Introduction to CALayers

---
+++ LayerFun/LayerFun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LayerFun
+//
+//  Created by chang on 15/2/26.
+//  Copyright (c) 2015年 chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LayerFun/LayerFun/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LayerFun
+//
+//  Created by chang on 15/2/26.
+//  Copyright (c) 2015年 chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
